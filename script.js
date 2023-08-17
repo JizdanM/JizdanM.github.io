de06ec23cb9db49897a619e5d748b46ff2edca55
mWarned <mihai.jizdan2021@gmail.com>
2023-08-17 19:23:13 +0300

    Added testing buttons and testing command line

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
-function inputFocus(input) {
-    if (input.placeholder === 'Search') {
-      input.placeholder = '';
+// Function to write the command when the button is pressed
+function writeText(input) {
+  const btnText = input.innerText;
+  const cmd = document.getElementById('usr_src');
+  cmd.value = '';
+  let i = 0;
+
+  function typeCharacter() {
+    if (i < btnText.length) {
+      cmd.value += btnText.charAt(i);
+      i++;
+      setTimeout(typeCharacter, 125);
+    }else {
+      // Fake the 'Enter' key press to trigger the search function
+      const enterClick = new KeyboardEvent('keydown', {
+        key: 'Enter'
+      })
+
+      cmdSearch(enterClick, cmd);
     }
   }
-  
-  function inputBlur(input) {
-    if (input.placeholder === '') {
-      input.placeholder = 'Search';
+
+  typeCharacter();
+}
+
+// Function for searching the page to display
+function cmdSearch(event, input) {
+  if (event.key === 'Enter'){
+    if (input.value === "Text type"){
+      const txtBrd = document.getElementById('infoSpan');
+      copyTextToDiv();
     }
+    else {
+      alert('Unknown command, please input a valid command.');
+    }
+
+    input.value = '';
   }
+}
+
+function copyTextToDiv() {
+  let outputDiv = document.getElementById('infoSpan');
+
+  fetch('content.txt')
+      .then(response => response.text())
+      .then(text => {
+        outputDiv.innerHTML = text;
+      })
+      .catch(error => {
+        console.error('Error loading text:', error);
+      });
+}
